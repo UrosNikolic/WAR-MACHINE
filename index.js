ea484f1a913b3334b778936eb2ec03e9c5cd584b
Uros Nikolic <unikolic94@gmail.com>
2019-06-19 18:24:57 +0200

    code refactoring

--- index.js
+++ index.js
@@ -1,38 +1,71 @@
 /**
- * strips `.` or `#` characters
+ * WAR MACHINE MODULE
+ * 
+ * This package is used for composing micro-fronteds.
+ * It gets your apps javascript and styles
+ * attaches it to the dom element you provided and your app gets
+ * initialized in that dom element
+ */
+
+const DEFAULT_VERSION = 'master'
+const SCRIPT_STATE = {
+    loaded: 'loaded',
+    complete: 'complete'
+}
+const ERROR_MESSAGES = {
+    elementNeeded: 'must include \'el\' property to scout options',
+    appPropertyNeeded: 'must include \'app\' property to scout options'
+}
+/**
+ * Strips `.` or `#` characters
  */
 function sanitized (input) {
-    if (input.length === 0) { return '' }
+    if (!input.length) { return '' }
+
     return input.toLowerCase()
         .replace(/^[#.]*/, '')
 }
 
 /**
- * converts an html5 tag format (e.g. my-component) to camelCase (e.g. myComponent)
+ * Converts an html5 tag format (e.g. my-component) to camelCase (e.g. myComponent)
  */
 function camelCase (input) {
     return sanitized(input)
-        .replace(/-(.)/g, function (match, group1) {
-            return group1.toUpperCase()
-        }).replace(/^[\S]/, function (match, group) {
+        .replace(/-(.)/g, (match, group) => {
+            return group.toUpperCase()
+        }).replace(/^[\S]/, (match) => {
             return match.toLowerCase()
         })
+}
+
+/**
+ * Get app version
+ */
+function getVersion (packageName) {
+    const version = window.__env &&
+        window.__env.pluginVersions &&
+        window.__env.pluginVersions[packageName]
+
+    return version || DEFAULT_VERSION
 }
 
 function loadScript(url, callback){
     const script = document.createElement('script')
     script.type = 'text/javascript'
 
-    if (script.readyState){  //IE
-        script.onreadystatechange = function(){
-            if (script.readyState == 'loaded' ||
-                script.readyState == 'complete'){
+    /**
+     * First part needed for IE support 'else' is for other browsers
+     */
+    if (script.readyState){
+        script.onreadystatechange = () => {
+            if (script.readyState === SCRIPT_STATE.loaded ||
+                script.readyState === SCRIPT_STATE.complete){
                 script.onreadystatechange = null
                 callback()
             }
         }
-    } else {  //Others
-        script.onload = function(){
+    } else {
+        script.onload = () => {
             callback()
         }
     }
@@ -50,29 +83,20 @@ function loadStyle(url, callback){
     document.getElementsByTagName('head')[0].appendChild(link)
 }
 
-const DEFAULT_VERSION = 'master'
-
-function getVersion (packageName) {
-    var version = window.__env &&
-        window.__env.pluginVersions &&
-        window.__env.pluginVersions[packageName]
-    if (version) {
-        return version
-    }
-    return DEFAULT_VERSION
-}
-
 /**
- * callback will be fired after each javascript script is loaded
+ * Callback will be fired after each javascript script is loaded
  */
-module.exports = function (options, cb) {
-    if (!options.el) throw new Error('must include \'el\' property to scout options')
-    if (!options.baseUrl) { // if no base url provided, require app and cdnUrl
-        if (!options.app) throw new Error('must include \'app\' property to scout options')
+module.exports = function (options, callback) {
+    if (!options.el) throw new Error(ERROR_MESSAGES.elementNeeded)
+    /**
+     * If no base url is provided, require app and cdnUrl
+     */
+    if (!options.baseUrl) {
+        if (!options.app) throw new Error(ERROR_MESSAGES.appPropertyNeeded)
         if (!options.version) options.version = getVersion(options.app) // no version set on this ENV default to 'master' build
     }
-    if (typeof cb !== 'function') {
-        cb = function () {
+    if (typeof callback !== 'function') {
+        callback = () => {
             const init = camelCase(options.el)
             typeof global[init] === 'function'
                 ? global[init](options.el)
@@ -81,7 +105,7 @@ module.exports = function (options, cb) {
     }
 
     /**
-     * when using the 'master' version, never cache so browsers automatically pick up new versions
+     * When using the 'master' version, never cache so browsers automatically pick up new versions
      * this default case is exclusively for dev/staging environments
      */
     let preventCache = ''
@@ -95,12 +119,16 @@ module.exports = function (options, cb) {
     if (typeof el === 'string') {
         el = document.querySelectorAll(el)
     }
-    if (typeof (el) !== undefined && el.length) { // element found on page inject assets
+
+    /**
+     * If binding element is found on page inject assets
+     */
+    if (typeof (el) !== undefined && el.length) {
         const baseUrl = options.baseUrl || options.cdnUrl + '/' + options.app + '/' + options.version
-        options.files.forEach(function (filename) {
+        options.files.forEach((filename) => {
             const fileUrl = baseUrl + '/' + filename + preventCache
             if (/.js$/.test(filename)) {
-                loadScript(fileUrl, cb)
+                loadScript(fileUrl, callback)
             } else if (/.css$/.test(filename)) {
                 loadStyle(fileUrl)
             }
